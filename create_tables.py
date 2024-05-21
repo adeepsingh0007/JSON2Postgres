aa5a1b316949576d9600490a3538cf9d424b2502
Anmol <30939239+adeepsingh0007@users.noreply.github.com>
2024-05-22 00:24:19 +0530

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,24 +4,24 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def create_database():
     """
-    - Creates and connects to the sparkifydb
-    - Returns the connection and cursor to sparkifydb
+    - Creates and connects to the tunewavedb
+    - Returns the connection and cursor to tunewavedb
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=tunewavedb user=twuser password=twpass")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    cur.execute("DROP DATABASE IF EXISTS tunewavedb")
+    cur.execute("CREATE DATABASE tunewavedb WITH ENCODING 'utf8' TEMPLATE template0")
 
     # close connection to default database
     conn.close()    
     
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=tunewavedb user=twuser password=twpass")
     cur = conn.cursor()
     
     return cur, conn
@@ -47,9 +47,9 @@ def create_tables(cur, conn):
 
 def main():
     """
-    - Drops (if exists) and Creates the sparkify database. 
+    - Drops (if exists) and Creates the TuneWave database. 
     
-    - Establishes connection with the sparkify database and gets
+    - Establishes connection with the TuneWave database and gets
     cursor to it.  
     
     - Drops all the tables.  
